2b63c08ad3aaacf35b7eba573d2735020989cb62
Christian Perez <christianperez@xpanxion.com>
2022-06-14 13:55:58 -0700

    This is the updated one

--- src/ProgramOne.java
+++ src/ProgramOne.java
@@ -3,11 +3,11 @@ public class ProgramOne {
     public static void main(String[] args) {
         int day = 7;
         Scanner input = new Scanner(System.in); //System.in is a standard input stream.
-        System.out.print("Enter first number- ");
+        System.out.print("Enter first number: ");
         int num = input.nextInt();
-        System.out.println(num * 7 + "days");
-        System.out.println(num * 10080+"Minutes");
-        System.out.println(num * 168+"Hours");
-        System.out.println(num * 604800+"seconds");
+        System.out.println(num * 7 + " days");
+        System.out.println(num * 10080+" Minutes");
+        System.out.println(num * 168+" Hours");
+        System.out.println(num * 604800+" seconds");
     }
 }
