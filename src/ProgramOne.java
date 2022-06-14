61b16fa3e30711b5a22811954c690dd86fcee0b6
Christian Perez <christianperez@xpanxion.com>
2022-06-14 14:02:38 -0700

    Fixed the message

--- src/ProgramOne.java
+++ src/ProgramOne.java
@@ -3,7 +3,7 @@ public class ProgramOne {
     public static void main(String[] args) {
         int day = 7;
         Scanner input = new Scanner(System.in); //System.in is a standard input stream.
-        System.out.print("Enter first number: ");
+        System.out.print("Enter number of weeks: ");
         int num = input.nextInt();
         System.out.println(num * 7 + " days");
         System.out.println(num * 10080+" Minutes");
