5d7b156c884d1469c57a588b847c409e360f9cfb
Christian Perez <christianperez@xpanxion.com>
2022-06-14 06:50:20 -0700

    Christian's Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main  {
+    /*
+    this is the first line
+    This is the second line*/
+    public static void main(String[] args) {
+        System.out.println("First line from app");
+        System.out.println("Second line from app");
+        System.out.println("Third line from app");
+
+    }
+} //This is a comment, need to use it every line or function
+// this way I can review for later and see what I was thinking
+//Line comment
+/*Block comment*/
+/*
+I need to start codeing, i dont wanna do this any more ahhhhh */
